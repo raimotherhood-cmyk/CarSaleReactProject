d7b0694a616fe8139c05d8d4e701b24f75f1e54f
Bhaskar Rai <bhaskar.rai@global.ntt>
2025-08-28 16:55:46 +0530

    ok

--- carsalereactproject/vite.config.js
+++ carsalereactproject/vite.config.js
@@ -1,41 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { ViteSitemap } from 'vite-plugin-sitemap';
-import { createHtmlPlugin } from 'vite-plugin-html';
-import { viteSSG } from 'vite-ssg/serialized-data';
+import plugin from '@vitejs/plugin-react';
 
-const routes = [
-    { path: '/', name: 'Home' },
-    { path: '/about', name: 'About' },
-    { path: '/contact', name: 'Contact' },
-];
-
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        viteSSG({ includedRoutes: () => routes }),
-        ViteSitemap({
-            baseUrl: 'https://happy-rock-0d9f35100.2.azurestaticapps.net/',
-            routes,
-            generateRobotsTxt: true,
-        }),
-        createHtmlPlugin({
-            minify: true,
-            inject: {
-                data: {
-                    title: 'Default Title',
-                    description: 'Default Description',
-                },
-            },
-        }),
-    ],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    react: ['react', 'react-dom', 'react-router-dom'],
-                },
-            },
-        },
-    },
-});
+    plugins: [plugin()],
+    server: {
+        port: 50984,
+    }
+})
